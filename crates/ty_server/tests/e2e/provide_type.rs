2bc1be93af1b3480b7bfdb647a8ff163a23965af
evgeny.bovykin <evgeny.bovykin@jetbrains.com>
2025-08-20 10:31:36 +0200

    Add a test for a generic specialized type

--- crates/ty_server/tests/e2e/provide_type.rs
+++ crates/ty_server/tests/e2e/provide_type.rs
@@ -68,3 +68,40 @@ b
 
     Ok(())
 }
+
+
+#[test]
+fn provide_generic_class_type() -> anyhow::Result<()> {
+    let workspace_root = SystemPath::new("src");
+    let foo = SystemPath::new("src/foo.py");
+    let foo_content = "\
+class A[T]:
+    i: T
+    def __init__(self, i: T):
+        self.i = i
+
+a = A(1)
+a
+";
+
+    let mut server = TestServerBuilder::new()?
+        .with_workspace(workspace_root, Some(ClientOptions::default()))?
+        .with_file(foo, foo_content)?
+        .enable_pull_diagnostics(true)
+        .build()?
+        .wait_until_workspaces_are_initialized()?;
+
+    server.open_text_document(foo, &foo_content, 1);
+    let provide_type_response = server.provide_type_request(
+        foo,
+        Range::new(
+            Position::new(6, 0),
+            Position::new(6, 1)
+        )
+    )?;
+
+    insta::assert_debug_snapshot!(provide_type_response);
+
+    Ok(())
+}
+
